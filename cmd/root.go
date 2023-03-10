22a209185d63f39658612f8f476bc2ebb48c8e31
Subhanshu Bansal <subhashu20135@iiitd.ac.in>
2023-03-10 16:32:48 +0530

    Added Version Command

--- cmd/root.go
+++ cmd/root.go
@@ -26,6 +26,11 @@ Visual Studio,
 GoLand, 
 CLion, 
 Android Studio`,
+	CompletionOptions: cobra.CompletionOptions{
+		DisableDefaultCmd: true,
+	},
+
+	Version: "1.0.0",
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -49,5 +54,5 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	//rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
